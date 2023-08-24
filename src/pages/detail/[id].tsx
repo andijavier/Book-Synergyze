934cfd1f22a39e2d3ba102fa78ba7d4ce547d02b
andijavier <andijavier442@gmail.com>
2023-08-24 21:31:11 +0700

    imageLinks

--- src/pages/detail/[id].tsx
+++ src/pages/detail/[id].tsx
@@ -53,9 +53,9 @@ export default function Detail() {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex flex-row">
           <div className="basis-1/2">
-            {book.volumeInfo.imageLinks && (
+            {book.volumeInfo?.imageLinks && (
                 <img
-                  src={book.volumeInfo.imageLinks.thumbnail}
+                  src={book.volumeInfo.imageLinks?.thumbnail}
                   alt={book.volumeInfo.title}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
